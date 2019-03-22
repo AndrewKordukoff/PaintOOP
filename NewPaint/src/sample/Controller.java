a654e59fb18aac6c4f2e0083ecff39b40c9afdcb
Andrew <workukoff1999@gmail.com>
2019-03-22 18:30:53 +0300

    paint foundation

---
+++ NewPaint/src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+public class Controller {
+    public void drawLine(ActionEvent actionEvent) {
+    }
+
+    public void mousePressed(KeyEvent keyEvent) {
+    }
+}
---
+++ NewPaint/src/sample/Shape/Figure.java
@@ -0,0 +1,7 @@
+package sample.Shape;
+
+import javafx.scene.canvas.Canvas;
+
+public abstract class Figure {
+    public abstract void draw(Canvas canvas, double x1, double y1, double x2, double y2);
+}
